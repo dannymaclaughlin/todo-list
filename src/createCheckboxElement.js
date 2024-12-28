3f7e477c7629108b814e0efb02923743490f655f
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-12-27 17:31:03 -0800

    Changed name of createTextElement function to createCheckboxElement.

--- src/createCheckboxElement.js
+++ src/createCheckboxElement.js
@@ -1,8 +1,9 @@
-export function createTextElement() {
+export function createCheckboxElement() {
     const taskList = document.getElementById('task-list');
     
     // test to see if this function is working
     const textDiv = document.createElement('div');
+    textDiv.classList.add('textDiv');
     const someText = document.createTextNode('Test text...');
     textDiv.append(someText)
     taskList.appendChild(textDiv);
--- src/newButtonListener.js
+++ src/newButtonListener.js
@@ -1,8 +1,8 @@
-import { createTextElement } from "./createCheckboxElement";
+import { createCheckboxElement } from "./createCheckboxElement";
 
 export function addNewButtonListener() {
     const newButton = document.getElementById('new-button');
     newButton.addEventListener("click", function () {
-        createTextElement();
+        createCheckboxElement();
     })
 }
