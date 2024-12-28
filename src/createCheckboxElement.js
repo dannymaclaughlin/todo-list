3d42553ac33b15e8cf281a4117f8a31a48b0c0c7
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-12-27 17:16:21 -0800

    Clicking on the New button now creates a new div then places the element inside of them. This leads to tasks listing in a column as expected.

--- src/createCheckboxElement.js
+++ src/createCheckboxElement.js
@@ -1,7 +1,11 @@
 export function createTextElement() {
     const taskList = document.getElementById('task-list');
+    
+    // test to see if this function is working
+    const textDiv = document.createElement('div');
     const someText = document.createTextNode('Test text...');
-    taskList.appendChild(someText);    
+    textDiv.append(someText)
+    taskList.appendChild(textDiv);
 };
 
 
