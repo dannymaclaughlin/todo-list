a39b7805dbefa16bbfcd3f668239bba50f881d91
dannymaclaughlin <dannymaclaughlin@gmail.com>
2025-01-04 19:12:09 -0800

    Added a description element to the task element and added styling. - Added a description element in addition to the task title element. - Grouped all elements excluding the checkbox element inside of one div in order to arrange them in a column. - Changed the names of some variable in order to better describe it's new role. - Added styling to the description to be smaller than the title of the task.

--- src/createCheckboxElement.js
+++ src/createCheckboxElement.js
@@ -1,24 +1,39 @@
 export function createCheckboxElement() {
     const taskList = document.getElementById('task-list');
 
-    // creates checkbox input element
-    const checkboxDiv = document.createElement('div');
-    checkboxDiv.classList.add('checkboxDiv');
+    // creates a div element for the entire checkbox/task element
+    const mainTaskDiv = document.createElement('div');
+    mainTaskDiv.classList.add('mainTaskDiv');
+
+    // creates checkbox element
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.id = 'newCheckbox';
     checkbox.classList = 'checkbox';
-    checkboxDiv.appendChild(checkbox);
+    mainTaskDiv.appendChild(checkbox); 
+
+    // creates div to house all elements excluding checkbox
+    const taskElementsDiv = document.createElement('div');
+    taskElementsDiv.classList.add('task-elements-div', 'strikethrough');
+    mainTaskDiv.appendChild(taskElementsDiv);
     
-    // creates label for checkbox element
+    // creates label element for checkbox element
     const label = document.createElement('label');
     label.htmlFor = 'newCheckbox';
+
+    // task title (creates title for label element)
     const newTaskName = prompt("Enter new task.")
     label.textContent = newTaskName;
-    label.classList = 'strikethrough'
-    checkboxDiv.appendChild(label);
+    label.classList.add('label-title');
+    taskElementsDiv.appendChild(label);
 
-    taskList.appendChild(checkboxDiv);
+    // task description
+    const taskDescriptionDiv = document.createElement('div')
+    taskDescriptionDiv.classList.add('task-description-div')
+    taskElementsDiv.appendChild(taskDescriptionDiv);
+    const newTaskDescription = prompt("Enter task description or notes.")
+    taskDescriptionDiv.textContent = newTaskDescription;
+    
+
+    taskList.appendChild(mainTaskDiv);
 };
-
-
