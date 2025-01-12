d091ec756a59e554b35cdee2d203c5e5a997f84c
dannymaclaughlin <dannymaclaughlin@gmail.com>
2025-01-12 14:35:14 -0800

    Clicking on the New button now calls on createCheckbox function.

--- src/newButtonListener.js
+++ src/newButtonListener.js
@@ -1,5 +1,6 @@
 import { createDiv } from "./components/createElement";
 import { appendElement } from "./components/appendElement";
+import { createCheckbox } from "./components/createElement";
 const taskList = document.getElementById('task-list');
 
 export function addNewButtonListener() {
@@ -7,6 +8,9 @@ export function addNewButtonListener() {
     newButton.addEventListener("click", function () {
         // creates a new div that will act as the container for each task
         const newTaskDiv = createDiv('taskContainer');
-        appendElement(taskList, newTaskDiv);
-    })
+        appendElement(newTaskDiv, taskList);
+
+        const newCheckbox = createCheckbox();
+        appendElement(newCheckbox, newTaskDiv);
+    });
 }
