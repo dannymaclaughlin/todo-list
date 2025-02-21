77fb2cf747c15945764a12291577850e7338d2a8
dannymaclaughlin <dannymaclaughlin@gmail.com>
2025-02-20 19:29:30 -0800

    Renamed application logic directory.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles/styles.css";
 // import { addNewButtonListener } from "./newButtonListener";
-import { addNewButtonListener } from "./components/application_logic/newButtonListener";
+import { addNewButtonListener } from "./components/application-logic/newButtonListener";
 
 document.addEventListener("DOMContentLoaded", function () {
     addNewButtonListener();
